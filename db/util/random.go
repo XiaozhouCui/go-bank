a26b4699ad45c490d8cf2ffa38d47e9821bf9a2e
Joe Cui <xiaozhou.cui@gmail.com>
2023-04-02 11:35:25 +1000

    Add helper functions to generate random things

---
+++ db/util/random.go
@@ -0,0 +1,48 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInit generates a random int64 between min and max.
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1) // min->max
+}
+
+// RandomString generates a random string of length n.
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)] // random index
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name.
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomAmount generates a random amount of money.
+func RandomAmount() int64 {
+	return RandomInt(0, 1000)
+}
+
+// RandomCurrency generates a random currency.
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
