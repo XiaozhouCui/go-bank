05395df1430ae801bb825a95015bae6307f91646
Joe Cui <xiaozhou.cui@gmail.com>
2023-07-23 11:09:01 +1000

    Add handler to create transfer

---
+++ api/transfer.go
@@ -0,0 +1,74 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	db "github.com/XiaozhouCui/go-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type transferRequest struct {
+	// binding is for validation
+	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createTransfer(ctx *gin.Context) {
+	var req transferRequest // incoming request
+	// validate the incoming request JSON
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	// validate currency for both accounts
+	if !server.validAccount(ctx, req.FromAccountID, req.Currency) {
+		return
+	}
+	if !server.validAccount(ctx, req.ToAccountID, req.Currency) {
+		return
+	}
+
+	// if req is valid, create transfer in db
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountID,
+		ToAccountID:   req.ToAccountID,
+		Amount:        req.Amount,
+	}
+
+	// create money transfer transaction
+	result, err := server.store.TransferTx(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// return the result
+	ctx.JSON(http.StatusOK, result)
+}
+
+func (server *Server) validAccount(ctx *gin.Context, accountId int64, currency string) bool {
+	account, err := server.store.GetAccount(ctx, accountId)
+	if err != nil {
+		// 2 error scenarios: 404 and 500
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+
+	// validate currency
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+	}
+
+	return true
+}
