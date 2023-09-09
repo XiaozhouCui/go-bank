7ead96601fa9266ff9a8d0015e589941e4743047
Joe Cui <xiaozhou.cui@gmail.com>
2023-09-09 10:56:57 +1000

    Add API endpoint to create new users

--- api/server.go
+++ api/server.go
@@ -22,6 +22,7 @@ func NewServer(store db.Store) *Server {
 	}
 
 	// add routes to router
+	router.POST("/users", server.createUser)
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
---
+++ api/user.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"net/http"
+	"time"
+
+	db "github.com/XiaozhouCui/go-bank/db/sqlc"
+	"github.com/XiaozhouCui/go-bank/db/util"
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+// exclude HashedPassword from User model
+type createUserResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
+// handler to create a new user
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest // incoming request
+	// validate the incoming request
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// if req is valid, create user in db
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		// handle pq error differently
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	// remove hashed password from user object to be returned in response
+	rsp := createUserResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
+
+	// return the user object as response
+	ctx.JSON(http.StatusOK, rsp)
+}
