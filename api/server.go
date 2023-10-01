b9738f1864f5924ba205fb317dce3e6a8d0df4c8
Joe Cui <xiaozhou.cui@gmail.com>
2023-10-01 12:15:11 +1000

    Add Paseto token maker when initializing HTTP server

--- api/server.go
+++ api/server.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/XiaozhouCui/go-bank/db/sqlc"
+	"github.com/XiaozhouCui/go-bank/db/util"
+	"github.com/XiaozhouCui/go-bank/token"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -9,12 +13,23 @@ import (
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	// initialize fields
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
@@ -30,7 +45,7 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-	return server
+	return server, nil
 }
 
 // Start runs the HTTP server on a specific address.
